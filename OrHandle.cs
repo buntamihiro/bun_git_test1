dcc01705c178cace3ca5009db3bd379368d84a6c
文民浩 <buntamihiro@gmail.com>
2020-12-01 16:06:37 +0900

    add Files
    Handlers

---
+++ OrHandle.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Npgsql;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+
+
+namespace SchemaInfo
+{
+    class OrHandle
+    {
+
+
+        public DataSet OracleCommand(OracleConnection con, string str)
+        {
+            OracleCommand command = new OracleCommand(str, con);
+            OracleDataAdapter adapter = new OracleDataAdapter();
+            adapter.SelectCommand = command;
+            DataSet ds = new DataSet();
+            //con.Open();
+            adapter.Fill(ds);
+            //con.Close();
+            return ds;
+        }
+
+
+
+
+        private DataTable SendCommand(OracleConnection con, string cmd)
+        {
+            DataSet pds1 = OracleCommand(con, cmd);
+
+            DataTable pdt1 = pds1.Tables[0];
+            return (pdt1);
+        }
+
+
+
+
+
+
+        public DataTable LoadTableInfoFromDB(OracleConnection con)
+        {
+            string cmd = "SELECT TC.TABLE_NAME, TC.COLUMN_NAME, TC.DATA_TYPE FROM USER_TAB_COLUMNS TC ORDER BY TC.TABLE_NAME, TC.COLUMN_NAME";
+            return (SendCommand(con, cmd));
+        }
+
+
+
+
+        public DataTable LoadSeqNameFromDB(OracleConnection con)
+        {
+            string cmd = "SELECT SEQUENCE_NAME FROM USER_SEQUENCES ORDER BY SEQUENCE_NAME";
+            return (SendCommand(con, cmd));
+        }
+
+
+        public DataTable LoadViewInfoFromDB(OracleConnection con)
+        {
+            string cmd = "SELECT V.VIEW_NAME, V.TEXT FROM USER_VIEWS V ORDER BY V.VIEW_NAME";
+            //string cmd = "SELECT views.VIEW_NAME AS view_name,views.TEXT AS definition FROM USER_VIEWS views ORDER BY views.VIEW_NAME";
+            //string cmd = "select view_name, text from user_views where view_name IN(SELECT OBJECT_NAME FROM USER_OBJECTS WHERE OBJECT_TYPE = 'VIEW')";
+            //string cmd = "select view_name, text from user_views where view_name = 'CHART_VW_SPC'";
+            //string cmd = "SELECT NAME,LINE,TEXT FROM USER_SOURCE WHERE NAME IN (SELECT OBJECT_NAME FROM USER_OBJECTS WHERE OBJECT_TYPE = 'VIEW') ORDER BY NAME,LINE";
+            return (SendCommand(con, cmd));
+        }
+
+
+
+        public DataTable LoadIndexInfoFromDB(OracleConnection con)
+        {
+            string cmd = "SELECT TABLE_NAME,COLUMN_NAME,INDEX_NAME FROM user_ind_columns order by table_name";
+            return (SendCommand(con, cmd));
+        }
+
+
+        public DataTable LoadFunctionInfoFromDB(OracleConnection con)
+        {
+            string cmd = "SELECT NAME,LINE,TEXT FROM USER_SOURCE WHERE NAME IN (SELECT OBJECT_NAME FROM USER_OBJECTS WHERE OBJECT_TYPE = 'FUNCTION') ORDER BY NAME,LINE";
+            return (SendCommand(con, cmd));
+        }
+
+
+        public DataTable LoadProcedureInfoFromDB(OracleConnection con)
+        {
+            string cmd = "SELECT NAME,LINE,TEXT FROM USER_SOURCE WHERE NAME IN (SELECT OBJECT_NAME FROM USER_OBJECTS WHERE OBJECT_TYPE = 'PROCEDURE') ORDER BY NAME,LINE";
+            return (SendCommand(con, cmd));
+        }
+
+
+
+        public DataTable LoadTriggerInfoFromDB(OracleConnection con)
+        {
+            string cmd = "SELECT NAME,LINE,TEXT FROM USER_SOURCE WHERE NAME IN (SELECT OBJECT_NAME FROM USER_OBJECTS WHERE OBJECT_TYPE = 'TRIGGER') ORDER BY NAME,LINE";
+            return (SendCommand(con, cmd));
+        }
+
+
+        public DataTable LoadPackageInfoFromDB(OracleConnection con)
+        {
+            //string cmd = "SELECT NAME,LINE,TEXT FROM USER_SOURCE WHERE NAME IN (SELECT OBJECT_NAME FROM USER_OBJECTS WHERE OBJECT_TYPE = 'PACKAGE') ORDER BY NAME,LINE";
+            string cmd = "SELECT NAME,LINE,TEXT FROM USER_SOURCE WHERE NAME IN (SELECT OBJECT_NAME FROM USER_OBJECTS WHERE OBJECT_TYPE = 'PACKAGE') ORDER BY NAME";
+            return (SendCommand(con, cmd));
+        }
+
+
+        public DataTable LoadSchedulerInfoFromDB(OracleConnection con)
+        {
+            
+            string cmd = "select job_name,owner,enabled,comments,logging_level,job_class,schedule_name,job_priority,job_weight from sys.dba_scheduler_jobs; ";
+            return (SendCommand(con, cmd));
+        }
+
+
+        public DataTable LoadObjectInfoFromDB(OracleConnection con,string objectName)
+        {
+            string cmd = "SELECT OBJECT_NAME FROM USER_OBJECTS WHERE OBJECT_TYPE = '" + objectName +"'";
+            return (SendCommand(con, cmd));
+        }
+
+
+    }
+}
---
+++ PgHandle.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Npgsql;
+using Oracle.DataAccess.Client;
+
+namespace SchemaInfo
+{
+    class PgHandle
+    {
+
+        private DataSet PostgresCommand(NpgsqlConnection con, string str)
+        {
+            NpgsqlCommand command = new NpgsqlCommand(str, con);                      
+            NpgsqlDataAdapter adapter = new NpgsqlDataAdapter();
+            adapter.SelectCommand = command;
+            DataSet ds = new DataSet();
+            con.Open();     // ★★★
+            adapter.Fill(ds);
+            con.Close();    // ★★★
+            return ds;
+        }
+
+
+
+
+
+
+
+        private DataTable SendCommand(NpgsqlConnection con, string cmd)
+        {
+            DataSet pds1 = PostgresCommand(con, cmd);
+
+            DataTable pdt1 = pds1.Tables[0];
+            return (pdt1);
+        }
+
+
+
+        public void ExecuteNonQuery(NpgsqlConnection conn, string sql)
+        {
+            using (NpgsqlCommand command = new NpgsqlCommand())
+            {
+                conn.ConnectionString = @"Server=localhost;Port=5432;User Id=admin;Password=admin;Database=TestDatabase;";
+
+                // トランザクションを開始します。
+                conn.Open();
+                NpgsqlTransaction transaction = conn.BeginTransaction(IsolationLevel.ReadCommitted);
+
+                try
+                {
+                    command.CommandText = sql;
+                    command.Connection = conn;
+                    command.Transaction = transaction;
+                    command.ExecuteNonQuery();
+
+                    //トランザクションをコミットします。
+                    transaction.Commit();
+                }
+                catch (System.Exception)
+                {
+                    //トランザクションをロールバックします。
+                    transaction.Rollback();
+                    throw;
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+
+
+
+
+
+        public DataTable LoadTableNameFromDB(NpgsqlConnection con)
+        {
+            string cmd = "SELECT relname FROM pg_stat_user_tables ORDER BY relname";
+            return (SendCommand(con,cmd));
+        }
+
+
+
+        public DataTable LoadTableDataTypeFromDB(NpgsqlConnection con,string tableName)
+        {
+            string cmd = "SELECT table_name,column_name,data_type FROM information_schema.columns WHERE table_name = '" + tableName + "' order by column_name";
+            return (SendCommand(con,cmd));
+        }
+
+
+
+        public DataTable LoadSeqNameFromDB(NpgsqlConnection con)
+        {
+            string cmd = "SELECT c.relname FROM pg_class c LEFT join pg_user u ON c.relowner = u.usesysid WHERE c.relkind = 'S';";
+            return (SendCommand(con, cmd));
+        }
+
+
+        public DataTable LoadViewInfoFromDB(NpgsqlConnection con)
+        {
+            string cmd = "SELECT viewname, definition FROM pg_views where schemaname = 'public';";
+            return (SendCommand(con, cmd));
+        }
+
+
+
+        public DataTable LoadIndexInfoFromDB(NpgsqlConnection con)
+        {
+            string cmd = "SELECT tablename, indexname, indexdef FROM pg_indexes where schemaname = 'public';";
+            return (SendCommand(con, cmd));
+        }
+
+
+        public DataTable LoadFunctionInfoFromDB(NpgsqlConnection con)
+        {
+            string cmd = "SELECT p.proname AS function_name,pg_get_function_arguments(p.oid) AS args, pg_get_functiondef(p.oid)AS func_def ";
+            cmd = cmd + "FROM(SELECT oid, *FROM pg_proc p WHERE NOT p.proisagg) p JOIN pg_namespace n ON n.oid = p.pronamespace WHERE n.nspname = 'public' ;";                       
+            return (SendCommand(con, cmd));
+        }
+
+
+        public DataTable LoadStatActivityFromDB(NpgsqlConnection con)
+        {
+            //string cmd = "SELECT datid,datname,pid,usesysid,usename,application_name,client_addr,client_port,backend_start,query_start,state_change,state,query ";
+            string cmd = "SELECT datid,datname,pid,usesysid,usename,application_name,client_addr,client_port,backend_start,query_start,state_change,state ";
+            cmd = cmd + "FROM pg_stat_activity";
+            return (SendCommand(con, cmd));
+        }
+
+
+    }
+
+
+}
